695b57dd21c3aba140e3c208d2bf9f4530576433
varshakaruturi <varshakaruturi@gmail.com>
2025-12-07 15:20:57 -0500

    Update final_exam_app.py

--- final_exam_app.py
+++ final_exam_app.py
@@ -4,8 +4,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
-import sklearn
-#from sklearn.preprocessing import StandardScaler # Required to unpickle the scaler
+from sklearn.preprocessing import StandardScaler # Required to unpickle the scaler
 
 # Load the trained model, scaler, feature columns, imputation medians, and categorical options
 with open("/content/drive/MyDrive/BUS 458 Final/final_exam_model.pkl", "rb") as file:
